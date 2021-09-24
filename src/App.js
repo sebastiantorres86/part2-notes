cc14bf963b568b8b4785cc952d07c0fdd939937a
sebastiantorres86 <seba.torres.dev@gmail.com>
2021-09-24 10:16:08 -0300

    part3-1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,27 @@ import Note from './components/Note'
 import Notification from './components/Notification'
 import noteService from './services/notes'
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>
+        Note app, Department of Computer Science, University of Helsinki 2021
+      </em>
+    </div>
+  )
+}
+
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened...')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   useEffect(() => {
     noteService.getAll().then(initialNotes => {
@@ -80,6 +96,7 @@ const App = () => {
         <input value={newNote} onChange={handleNoteChange} />
         <button type='submit'>save</button>
       </form>
+      <Footer />
     </div>
   )
 }
